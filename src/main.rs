dbded55ce721993b6c588e9b67fa562b871366ea
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-11 23:15:12 -0500

    Add: collect query, filename, and print results to the console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// example:$ cargo run searchstring example-filename.txt
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+    print!("Searching for {}", query);
+    println!("In file {}", filename);
 }
