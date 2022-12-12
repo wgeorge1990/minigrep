d3a182d314928079b3f68e008b796ccd0293b0a0
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-11 23:18:22 -0500

    Add: Step commenting

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@
 use std::env;
 
 fn main() {
+    //collect arguments
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
+
+    //store args into variables
     let query = &args[1];
     let filename = &args[2];
+    
+    //printing the variables to console for development
     print!("Searching for {}", query);
     println!("In file {}", filename);
 }
