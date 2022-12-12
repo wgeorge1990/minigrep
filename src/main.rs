ac35ff99cff38e5073a836568382a9006baadbd6
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-11 23:26:05 -0500

    Add: read file functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // example:$ cargo run searchstring example-filename.txt
 use std::env;
+use std::fs;
 
 fn main() {
     //collect arguments
@@ -11,6 +12,13 @@ fn main() {
     let filename = &args[2];
     
     //printing the variables to console for development
-    print!("Searching for {}", query);
+    println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    //read file, save text to var, and print to console
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong with reading the file");
+    println!("With text:\n{}", contents);
+
+
 }
