13afd64f2ed9e8a8bd6f46ae79bad7778c22e177
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-12 09:38:40 -0500

    Refactor: break argument parsing into seperate function

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,21 @@ fn main() {
     println!("{:?}", args);
 
     //store args into variables
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
     
     //printing the variables to console for development
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for => {}", query);
+    println!("In file => {}", filename);
 
     //read file, save text to var, and print to console
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong with reading the file");
     println!("With text:\n{}", contents);
 
+}
 
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
 }
