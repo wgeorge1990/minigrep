31367e92c2301c0438474c583a17952bdd5e0dd7
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-14 08:50:23 -0500

    Refactor: add more specific error handling by returning Result type and then adding unwrap_or_else closure to the Config::new which returns the error and process code

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 // example:$ cargo run searchstring example-filename.txt
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
-    //collect arguments
+    //collect arguments from command line
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    //store args into variables
-    let config = Config::new(&args);
-    
+    //create Config struct with parsed arguments
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
     //printing the variables to console for development
     println!("Searching for => {}", config.query);
     println!("In file => {}", config.filename);
@@ -26,9 +30,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
-     Config { query, filename}
+     Ok(Config { query, filename})
     }
 }
