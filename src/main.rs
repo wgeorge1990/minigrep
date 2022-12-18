389a05533bdaca02d32e2e18abbb3307d8d6108c
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-12-17 21:29:38 -0500

    TDD: start test driven development for search function. Start with empty failing test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 // example:$ cargo run searchstring example-filename.txt
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     //collect arguments from command line
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
 
     //create Config struct with parsed arguments
     let config = Config::new(&args).unwrap_or_else(|err| {
@@ -15,35 +14,8 @@ fn main() {
         process::exit(1);
     });
 
-    //printing the variables to console for development
-    println!("Searching for => {}", config.query);
-    println!("In file => {}", config.filename);
-
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-//read file, save text to var, and print to console
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-     Ok(Config { query, filename})
     }
 }
